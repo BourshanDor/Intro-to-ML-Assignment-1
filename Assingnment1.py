0e8c4d0b3c2404b0e7c07c60a242a14b68875968
BourshanDor <92336611+BourshanDor@users.noreply.github.com>
2023-03-16 21:56:18 +0200

    finish

--- Assingnment1.py
+++ Assingnment1.py
@@ -1,14 +1,9 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-# from scipy.stats import t
-# from scipy.stats import poisson
-
-
 
 def main(): 
-
-    print("Generate an N x n matrix of samples from Bernoulli(p) ")
+    # print("Generate an N x n matrix of samples from Bernoulli(p) ")
     # p = float(input("Enter bernoulli parameter:\n"))
     # N = int(input("Enter N:\n"))
     # n = int(input("Enter n:\n"))
@@ -23,7 +18,6 @@ def main():
     hoeffding_bound = [2*math.pow(math.e, (-2)*N*(math.pow(epsilon_random[i],2))) for i in range(len(epsilon_random))] 
     plot_care(epsilon_random, y_axis, hoeffding_bound) 
 
-    
 
 def empirial_epsilon(result_mean, num_of_epsilon,) : 
     epsilon_random = np.linspace(0,1,num_of_epsilon)
@@ -44,26 +38,5 @@ def plot_care(x,y, hoeffding_bound) :
     plt.ylabel("probability |X - 1/2| > epsilon")
     plt.show()
     
-        
-
-
-
-#     x = np.linspace(mean - 3*STD, mean + 3*std, 100)
-#     plt.plot(x, stats.norm.pdf(x, mean, std),label="Norm")
-
-#     plt.legend()
-#     plt.show()
-
-# def q3e6(grades, mean): 
-#     sum = 0 
-#     for x in grades: 
-#         sum +=  (x-mean)**2
-#     print(sum/14)
-
-# def clalPPF ():
-#     print(stats.norm.ppf(0.975)*10*(1/math.sqrt(15)))
-
-
-
 if __name__ == "__main__":
     main()
