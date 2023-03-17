2834eb99534dcf76cac0b811fa9fd72ed36c7fed
BourshanDor <92336611+BourshanDor@users.noreply.github.com>
2023-03-17 09:22:44 +0200

    changes

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -11,33 +11,31 @@ def main():
     N = 200000 
     n = 20 
     bernoulli_matrix = create_bernoullii_matrix(p,N,n) 
-    print(len(bernoulli_matrix))
-    result_mean = [np.mean(bernoulli_matrix[i]) for i in range(N)]
+    print(bernoulli_matrix)
+    result_mean = [np.mean(bernoulli_matrix[i]) for i in range(n)]
+    print(result_mean)
     epsilon_random, y_axis = empirial_epsilon(result_mean, 50)
-    
-    hoeffding_bound = [2*math.pow(math.e, (-2)*n*(math.pow(epsilon_random[i],2))) for i in range(len(epsilon_random))] 
+    hoeffding_bound = [2*math.pow(math.e, (-2)*N*(math.pow(epsilon_random[i],2))) for i in range(len(epsilon_random))] 
     plot_care(epsilon_random, y_axis, hoeffding_bound) 
 
 
-def empirial_epsilon(result_mean: list, num_of_epsilon: int ) -> tuple(2) : 
+def empirial_epsilon(result_mean, num_of_epsilon,) : 
     epsilon_random = np.linspace(0,1,num_of_epsilon)
     y_axis = [] 
     for j in range(num_of_epsilon) : 
-        y = [1 if (abs(result_mean[i] - 0.5 ) > epsilon_random[j]) else 0  for i in range(len(result_mean))] 
+        y= [1 if (abs(result_mean[i] - 0.5 ) > epsilon_random[j]) else 0  for i in range(len(result_mean))] 
         y_axis.append(np.mean(y)) 
-    
     return epsilon_random, y_axis
 
 def create_bernoullii_matrix(p: int,N:int ,n:int ) -> np.array:
-    return np.random.binomial(1,p, size=(N,n))
+    return np.random.binomial(1,p, size=(n,N))
 
 def plot_care(x,y, hoeffding_bound) : 
-    plt.plot(x, y, label="Empirical distribution")
-    plt.plot(x, hoeffding_bound,  label="Hoeffding bound")
+    plt.plot(x, y, label="Empirial")
+    plt.plot(x, hoeffding_bound, label="Hoeffding bound")
     plt.title("Hoeffding bound")
     plt.xlabel("epsilon")
     plt.ylabel("probability |X - 1/2| > epsilon")
-    plt.legend()
     plt.show()
     
 if __name__ == "__main__":
